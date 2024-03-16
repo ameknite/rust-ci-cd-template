202254fb0b065815dce01db2f96383d3d417d710
AmeKnite <104745335+ameknite@users.noreply.github.com>
2024-03-16 02:59:56 -0600

    fix typos

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: CC0-1.0 OR MIT OR Apache-2.0
-// -> todo! remove this notices
+// -> todo! remove this notice
 
 fn main() {
     println!("Hello, world!");
-    todo!("he")
 }
